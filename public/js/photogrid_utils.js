38f30b8272cb5719777435d79a53c5c411ed4f27
Jae Sung Kim <odinducky@gmail.com>
2015-01-01 17:15:42 -0800

    first commit

---
+++ public/js/photogrid_utils.js
@@ -0,0 +1,35 @@
+function showStatus(msg,delay){
+		$('.status').hide().html(msg).fadeIn(300).delay(delay).fadeOut(500); // show message that fades in to appear then fades out after a delay
+}
+
+function ajax(config){
+			this.method = config.method || 'GET';
+			this.payload = config.payload || null;
+			var xhr = new XMLHttpRequest();
+			xhr.open(this.method, config.url, true);
+			xhr.upload.addEventListener("progress", function(e){
+				config.progress(e);
+			});
+			xhr.addEventListener("load", function(){
+				config.success(xhr);
+			});
+			xhr.addEventListener("error", config.error);
+			xhr.send(this.payload);
+}
+
+$(function(){
+		$(document).on('change', '.uploadPic', function(e){
+			var ext = this.value.match(/\.([^\.]+)$/)[1].toLowerCase(); // literal period "\."  logical grouping ()$. the $ indicates END of string.
+																		// [] is character set. in the set any character OTHER than "." is allowed (^.)
+																		// one or more times.  thus this regex matches file extensions
+			var permit = ['jpg','gif','png'];
+			if(permit.indexOf(ext)>-1){
+				showStatus('Ready to Upload !', 600);
+			} else {
+				showStatus('Your Chosen File Is Not Permitted !! Please pick JPG, GIF or PNG files only !', 4000);
+				$(this).val('');
+			}
+		})
+
+
+})
